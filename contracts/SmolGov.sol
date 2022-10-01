d857d0d1cedf7d43e43c3a6b6115621b69fece96
ghoul-sol <ghoul.sol@pm.me>
2022-10-01 10:08:49 -0500

    SmolGov

--- contracts/SmolGov.sol
+++ contracts/SmolGov.sol
@@ -2,14 +2,41 @@
 pragma solidity 0.8.7;
 
 import '@openzeppelin/contracts/token/ERC20/ERC20.sol';
+import '@openzeppelin/contracts/token/ERC721/extensions/IERC721Enumerable.sol';
 
 import './SmolBrain.sol';
 
+interface ISmolFarm {
+    function tokensOfOwner(address _collection, address _owner) external view returns (uint256[] memory);
+}
+
+interface IGym {
+    function isAtGym(uint256 _tokenId) external view returns (bool);
+}
+
+interface ISmolRacing {
+    function smolsOfOwner(address _collection, address _owner) external view returns (uint256[] memory);
+}
+
 contract SmolGov is ERC20 {
     SmolBrain public smolbrain;
+    ISmolFarm public smolfarm;
+    IERC721Enumerable public smolBodies;
+    IGym public smolgym;
+    ISmolRacing public smolracing;
 
-    constructor(address _smolbrain) ERC20("Smol Governance", "gSMOL") {
+    constructor(
+        address _smolbrain,
+        address _smolfarm,
+        address _smolBodies,
+        address _smolgym,
+        address _smolracing
+    ) ERC20("Smol Governance", "gSMOL") {
         smolbrain = SmolBrain(_smolbrain);
+        smolfarm = ISmolFarm(_smolfarm);
+        smolBodies = IERC721Enumerable(_smolBodies);
+        smolgym = IGym(_smolgym);
+        smolracing = ISmolRacing(_smolracing);
     }
 
     function totalSupply() public view override returns (uint256) {
@@ -17,6 +44,14 @@ contract SmolGov is ERC20 {
     }
 
     function balanceOf(address _account) public view override returns (uint256 balanceAtSchool) {
+        return
+            getSchoolBalance(_account) +
+            getFarmBalance(_account) +
+            getGymBalance(_account) +
+            getRacingBalance(_account);
+    }
+
+    function getSchoolBalance(address _account) public view returns (uint256 balanceAtSchool) {
         uint256 balance = smolbrain.balanceOf(_account);
         School school = smolbrain.school();
         for (uint256 i = 0; i < balance; i++) {
@@ -27,11 +62,27 @@ contract SmolGov is ERC20 {
         }
     }
 
-    function _beforeTokenTransfer(
-        address from,
-        address to,
-        uint256 amount
-    ) internal override {
+    function getFarmBalance(address _account) public view returns (uint256 balanceAtFarm) {
+        uint256[] memory tokensInFarm = smolfarm.tokensOfOwner(address(smolbrain), _account);
+        return tokensInFarm.length;
+    }
+
+    function getGymBalance(address _account) public view returns (uint256 balanceAtGym) {
+        uint256 balance = smolBodies.balanceOf(_account);
+        for (uint256 i = 0; i < balance; i++) {
+            uint256 tokenId = smolBodies.tokenOfOwnerByIndex(_account, i);
+            if (smolgym.isAtGym(tokenId)) {
+                balanceAtGym++;
+            }
+        }
+    }
+
+    function getRacingBalance(address _account) public view returns (uint256 balanceAtRacing) {
+        uint256[] memory tokensInRacing = smolracing.smolsOfOwner(address(smolbrain), _account);
+        return tokensInRacing.length;
+    }
+
+    function _beforeTokenTransfer(address, address, uint256) internal pure override {
         revert("Non-transferable");
     }
 }
--- deploy/SmolGov.deploy.ts
+++ deploy/SmolGov.deploy.ts
@@ -6,10 +6,16 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deploy, read, execute } = deployments;
   const { deployer } = await getNamedAccounts();
 
+  const smolbrain = "0x6325439389E0797Ab35752B4F43a14C004f22A9c";
+  const smolfarm = "0xC2E007C61319fcf028178FAB14CD6ED6660C6e86";
+  const smolbodies = "0x17DaCAD7975960833f374622fad08b90Ed67D1B5";
+  const smolgym = "0x66299ecC614b7A1920922bBa7527819c841174BD";
+  const smolracing = "0xEc895f620D1c103d5Bbc85CcE3b623C958Ce35cC";
+
   const SmolGov = await deploy('SmolGov', {
     from: deployer,
     log: true,
-    args: [(await deployments.get('SmolBrain')).address]
+    args: [smolbrain, smolfarm, smolbodies, smolgym, smolracing]
   })
 };
 export default func;
